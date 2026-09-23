9317da173070c45c0fe477928242bd5acdc6d867
dfcut8 <dfcut8@proton.me>
2026-09-23 10:27:18 -0400

    controls

--- Sources/LearnSwift/ControlStatements.swift
+++ Sources/LearnSwift/ControlStatements.swift
@@ -3,5 +3,9 @@ class CoolControlStatements {
         for i in 0...12 {
             print(i)
         }
+
+        for i in stride(from: 12, to: 0, by: -1) {
+            print(i)
+        }
     }
 }
